88ef4572c9c4ae213a7a16cde21e0a970b47be52
TheExample67 <silvan.schiess@gmail.com>
2019-03-07 16:03:19 +0100

    made the code more compact.

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ ctx = canvas.getContext('2d');
 
 var mouseX;
 var mouseY;
-var Sin_Points = [];
-var Cos_Points = [];
-var Tan_Points = [];
-var No_of_Curve_Points = 10;
+var Points = [];
 var ColorTheme = ['#287682', '#14ABBD', '#FFAA00', '#FF6000', '#FF3800'];
-var DefaultCircle = [];
+var SinColor = ColorTheme[Math.floor(Math.random() * ColorTheme.length)];
+var CosColor =ColorTheme[Math.floor(Math.random() * ColorTheme.length)];
+var TanColor = ColorTheme[Math.floor(Math.random() * ColorTheme.length)];
+var DefaultCircle;
 
 
 
@@ -16,7 +16,7 @@ var DefaultCircle = [];
 window.addEventListener('load', resize, false);
 window.addEventListener('resize', resize, false);
 window.addEventListener('mousemove', function (evt) {
-     var mousePos = getMousePos(canvas, evt);
+    var mousePos = getMousePos(canvas, evt);
     mouseX = mousePos.x;
     mouseY = mousePos.y;
 }, false);
@@ -37,36 +37,37 @@ function resize() {
 
 //Classes
 class Curve_Point {
-    constructor(x,y) {
+    constructor(x, y,color) {
         //Position
         this.x = x;
         this.y = y;
+        this.color = color;
     }
-    draw(){
+    draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
         ctx.fill();
     }
-    update(){
+    update() {
         this.x = this.x + 1;
     }
 }
 
 class Default_Circle {
-    constructor (x,y,radius,weight,color,tracer) {
+    constructor(x, y, radius, color, tracer) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.weight = weight;
         this.color = color;
         this.tracer = tracer;
     }
     draw() {
+        this.update();
         //Draw Circle
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.StrokeStyle = this.color;
-        //ctx.context.lineWidth= this.weight;
         ctx.stroke();
 
         //Draw Tracer
@@ -74,7 +75,7 @@ class Default_Circle {
         var xpos = this.x + this.radius * Math.cos(this.tracer);
         var ypos = this.y + this.radius * Math.sin(this.tracer);
 
-        ctx.arc(xpos, ypos, this.radius/10,0,Math.PI*2);
+        ctx.arc(xpos, ypos, this.radius / 10, 0, Math.PI * 2);
         ctx.fillStyle = this.color;
         ctx.fill();
 
@@ -85,66 +86,42 @@ class Default_Circle {
 }
 
 function GenerateDefaultCircle() {
-    DefaultCircle = [];
-    DefaultCircle.push(new Default_Circle(canvas.width/10,canvas.height/2,canvas.width/10,'2pt',ColorTheme[Math.floor(Math.random() * ColorTheme.length)],0));
+    DefaultCircle = new Default_Circle(canvas.width / 10, canvas.height / 2, canvas.width / 10, ColorTheme[Math.floor(Math.random() * ColorTheme.length)], 0);
 }
 
-//
 function GenerateCurve_Point() {
-    DefaultCircle.forEach(trace => {
-        var ypos =  trace.y +trace.radius * Math.sin(trace.tracer)
-        Sin_Points.push(new Curve_Point(canvas.width/2,ypos));
-        var ypos =  trace.y +trace.radius * Math.cos(trace.tracer)
-        Cos_Points.push(new Curve_Point(canvas.width/2,ypos));
-        var ypos =  trace.y +trace.radius * Math.tan(trace.tracer)
-        if (ypos < canvas.height) {
-            Tan_Points.push(new Curve_Point(canvas.width/2,ypos));
-        }
-    });   
+    StartingXPos = canvas.width/4;
+    var ypos = DefaultCircle.y + DefaultCircle.radius * Math.sin(DefaultCircle.tracer)
+    Points.push(new Curve_Point(StartingXPos, ypos,SinColor));
+    var ypos = DefaultCircle.y + DefaultCircle.radius * Math.cos(DefaultCircle.tracer)
+    Points.push(new Curve_Point(StartingXPos, ypos,CosColor));
+    var ypos = DefaultCircle.y + DefaultCircle.radius * Math.tan(DefaultCircle.tracer)
+    if (ypos < canvas.height) {
+        Points.push(new Curve_Point(StartingXPos, ypos,TanColor));
+    }
 }
 
 
 //General Funcitons
 function init() {
     GenerateDefaultCircle();
+
 };
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    DefaultCircle.forEach(e => {
-        e.update();
-        e.draw();
-    });
+    DefaultCircle.draw();
     GenerateCurve_Point();
-    Sin_Points.forEach(e => {
-        
+    Points.forEach(e => {
+
         if (e.x < canvas.width) {
             e.update();
             e.draw();
         } else {
-            Sin_Points.shift();
-        }
-    });
-    Cos_Points.forEach(e => {
-        
-        if (e.x < canvas.width) {
-            e.update();
-            e.draw();
-        } else {
-            Cos_Points.shift();
-        }
-    });
-    Tan_Points.forEach(e => {
-        
-        if (e.x < canvas.width) {
-            e.update();
-            e.draw();
-        } else {
-            Tan_Points.shift();
+            Points.shift();
         }
     });
 };
 
 init();
-setInterval(draw,0);
+setInterval(draw, 0);
